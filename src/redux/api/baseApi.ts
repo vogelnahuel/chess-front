4691c97a0e170df6439353fca6eabbb39c2d276c
vogelnahuel <vogelnahuel@gmail.com>
2025-01-18 18:29:45 -0300

    creando semilla

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { config } from "./config";
-import { getTokenFromLocalStorage } from "../../utils/localStorage";
+import { getLocalStorage } from "../../utils/localStorage";
 
 // Definimos la API con su configuración base, sin ningún endpoint.
 // Los endpoints serán definidos luego en archivos separados.
@@ -9,7 +9,7 @@ export const baseApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: config.apiUrl,
     prepareHeaders: async (headers) => {
-      const token = getTokenFromLocalStorage();
+      const token = getLocalStorage("token");
 
       if (!token) return;
       headers.set("authorization", `Bearer ${token}`);
