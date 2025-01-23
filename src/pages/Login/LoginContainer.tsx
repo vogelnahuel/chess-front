31ff2b2487f5e3e513c71e5c1b601b99839f7d7a
vogelnahuel <vogelnahuel@gmail.com>
2025-01-23 18:06:29 -0300

    redirect

--- src/pages/Login/LoginContainer.tsx
+++ src/pages/Login/LoginContainer.tsx
@@ -5,10 +5,12 @@ import { Login } from './Login';
 import { useLoginMutation } from '../../services/login/loginApi';
 import { saveLocalStorage } from '../../utils/localStorage';
 import { setLoginData } from '../../redux/slice/loginSlice';
+import { useNavigate } from 'react-router-dom';
 
 export const LoginContainer: React.FC = () => {
   const dispatch = useDispatch();
   const [login, { isLoading }] = useLoginMutation();
+  const navigate =  useNavigate();
 
   const handleLogin = async (values: { email: string; password: string }) => {
     try {
@@ -16,6 +18,8 @@ export const LoginContainer: React.FC = () => {
 
       saveLocalStorage("token", response.data.accessToken);
       dispatch(setLoginData(response.data.accessToken));
+      navigate("/dashboard")
+
     } catch (error) {
       alert('Error al iniciar sesión: ' + (error as Error).message);
     }
